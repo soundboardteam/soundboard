a86cb88f9e4db4c2a03fa66858a40a3ff48b5fe4
rithikgavvala <gavvalarithik@gmail.com>
2023-09-26 23:18:06 -0700

    update index

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App: React.FC = () => {
     return (
         <main className="App">
-            <h1>My App</h1>
+            <h1>Soundboard</h1>
         </main>
     )
 }
