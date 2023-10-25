eb262ada65e8d8f7f48d4097c6bee7a37e4e524c
rithikgavvala <gavvalarithik@gmail.com>
2023-10-24 18:41:00 -0700

    update ratings

--- server/dist/js/models/rating.js
+++ server/dist/js/models/rating.js
@@ -1,2 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const ratingSchema = new mongoose_1.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    ratingValue: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    associatedJob: {
+        type: String,
+        required: true,
+    },
+});
--- server/src/models/rating.ts
+++ server/src/models/rating.ts
@@ -1,2 +1,25 @@
 import { IRating } from '../types/rating'
 import { model, Schema } from 'mongoose'
+
+const ratingSchema: Schema = new Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    ratingValue: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    associatedJob: {
+        type: String,
+        required: true,
+    },
+})
