a634b3929064e00731958fb21396bb8ac361ce97
rithikgavvala <gavvalarithik@gmail.com>
2023-12-11 19:07:01 -0800

    service account key issue

--- server/src/config/firebase-config.ts
+++ server/src/config/firebase-config.ts
@@ -1,9 +1,8 @@
+import { serviceAccountKey } from './serviceAccountKey'
 const firebaseAdmin = require('firebase-admin')
 
-const serviceAccount = require('./serviceAccountKey.json')
-
 firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serviceAccount),
+    credential: firebaseAdmin.credential.cert(serviceAccountKey),
 })
 
 export default firebaseAdmin
